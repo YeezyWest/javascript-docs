a5441de0b8c1c4d5aec17f98434fb3c3e0ebe706
Yusuf olatunji adeshina <yadeshina57@gmai.com>
2024-03-01 18:34:21 +0100

    first commit

---
+++ 01-variable-data-types/01-console/script.js
@@ -0,0 +1,17 @@
+console.log(100);
+
+console.log(20, 'hello world', true);
+
+const a = 50;
+
+console.log(a);
+
+console.warn("warning")
+console.error("error")
+console.table({name: "yusuf", nick: "west"})
+
+console.group('simple');
+console.warn("warning")
+console.error("error")
+console.table({name: "yusuf", nick: "west"})
+console.groupEnd();
